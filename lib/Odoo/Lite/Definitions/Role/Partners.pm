689c1f10fd6b4f960064e6717e4358c2ce73504b
Brad Haywood <brad.haywood@opusvl.com>
2014-08-29 14:30:44 +0100

    employees definition returns a proper Odoo::Lite::Result

--- lib/Odoo/Lite/Definitions/Role/Partners.pm
+++ lib/Odoo/Lite/Definitions/Role/Partners.pm
@@ -17,13 +17,20 @@ has_def 'companies' => (
 has_def 'employees' => (
     as      => 'res.partner',
     default => sub {
-        my ($self, $company) = @_;
+        my ($self, $company, $fields) = @_;
+        $fields //= ['name', 'email'];
         unless ($company->{is_company}) {
             warn $company->{name} . " is not a valid company!";
             return 0;
         }
 
         if ($self->proto eq 'jsonrpc') {
+            if (ref $company eq 'HASH' and $company->{child_ids}) {
+                return $self->search(
+                    $fields,
+                    [ map { ['id', '=', $_] } @{$company->{child_ids}} ],
+                );
+            }
         }
 
         return @{$company->{child_ids}};
