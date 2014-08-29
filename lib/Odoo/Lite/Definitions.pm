013a776a5c9f2b2c3479ab53588d125b7fd314e5
Brad Haywood <brad.haywood@opusvl.com>
2014-08-29 11:23:44 +0100

    Added definition to obtain any active sales orders

--- lib/Odoo/Lite/Definitions.pm
+++ lib/Odoo/Lite/Definitions.pm
@@ -29,5 +29,20 @@ has_def 'employees' => (
     },
 );
 
+has_def 'active_sales_orders' => (
+    as      => 'sale.order',
+    default => sub {
+        my ($self, $args) = @_;
+        unless ($args and ref $args eq 'HASH' and $args->{id}) {
+            die "active_sales_orders() expects hash ref with 'id'";
+        }
+
+        return $self->search(
+            ['state', 'user_id', 'partner_id', 'name', 'amount_total', 'date_order'],
+            [['state', 'not in', ['draft', 'sent', 'cancel']], ['user_id', '=', $args->{id}]],
+        );
+    },
+);
+
 1;
 __END__
